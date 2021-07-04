25a1d5164ea3337bfceb6237d8604380ab74c960
Austin Hicks <ahicks@ahicks.io>
2021-07-04 16:12:16 -0700

    Make cloning/drop properly infallible

--- synthizer/src/handle.rs
+++ synthizer/src/handle.rs
@@ -4,17 +4,17 @@ use crate::errors::*;
 
 pub struct Handle(pub(crate) syz_Handle);
 
-impl Handle {
-    pub fn try_clone(&self) -> Result<Handle> {
-        check_error(unsafe { syz_handleIncRef(self.0) })?;
-        Ok(Handle(self.0))
-    }
-}
-
 impl Drop for Handle {
     fn drop(&mut self) {
         check_error(unsafe { syz_handleDecRef(self.0) })
             .expect("Dropping handles should not error");
+    }
+}
+
+impl Clone for Handle {
+    fn clone(&self) -> Handle {
+        check_error(unsafe { syz_handleIncRef(self.0) }).expect("Incrementing reference counts should never error");
+        Handle(self.0)
     }
 }
 
