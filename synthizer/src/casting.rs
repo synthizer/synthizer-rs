d91612684c8f05f5d1097c067d08f143db3145fd
Austin Hicks <ahicks@ahicks.io>
2021-07-10 10:37:30 -0700

    Rename the cast macro back to downcast

--- synthizer/src/casting.rs
+++ synthizer/src/casting.rs
@@ -1,8 +1,8 @@
 //! Implement casting (upcasting and downcasting) for Synthizer objects. Allows converting any object to a handle, and upcasting/downcasting along the inheritance hierarchy.
 use crate::*;
 
-/// Set up the list of base classes and casting functionality.  The
-macro_rules! cast {
+/// Set up the list of base classes and casting functionality.
+macro_rules! downcast {
     // Generate downcasting functionality to convert `t1` to `t2`.
     ($t1: ty, $t2: ty) => {
         impl From<$t1> for $t2 {
@@ -14,19 +14,19 @@ macro_rules! cast {
 
     // And this one to support running it on a whole list of types, from most to least derived.
     ($t1: ty, $t2: ty, $($tys: ty),+) => {
-        cast!($t1, $t2);
-        cast!($t1, $($tys),*);
+        downcast!($t1, $t2);
+        downcast!($t1, $($tys),*);
     }
 }
 
-cast!(Buffer, Handle);
+downcast!(Buffer, Handle);
 
-cast!(Generator, Handle);
-cast!(BufferGenerator, Generator, Handle);
-cast!(NoiseGenerator, Generator, Handle);
-cast!(StreamingGenerator, Generator, Handle);
+downcast!(Generator, Handle);
+downcast!(BufferGenerator, Generator, Handle);
+downcast!(NoiseGenerator, Generator, Handle);
+downcast!(StreamingGenerator, Generator, Handle);
 
-cast!(Source, Handle);
-cast!(DirectSource, Source, Handle);
-cast!(PannedSource, Source, Handle);
-cast!(Source3D, Source, Handle);
+downcast!(Source, Handle);
+downcast!(DirectSource, Source, Handle);
+downcast!(PannedSource, Source, Handle);
+downcast!(Source3D, Source, Handle);
