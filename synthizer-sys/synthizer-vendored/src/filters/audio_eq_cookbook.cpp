932231a840838c521adb7f9c8f33922484eb94c6
Austin Hicks <ahicks@ahicks.io>
2022-03-06 12:06:21 -0800

    Update Synthizer to 0.11.7

--- synthizer-sys/synthizer-vendored/src/filters/audio_eq_cookbook.cpp
+++ synthizer-sys/synthizer-vendored/src/filters/audio_eq_cookbook.cpp
@@ -5,46 +5,14 @@
 #include "synthizer/filter_design.hpp"
 #include "synthizer/math.hpp"
 
-#pragma GCC diagnostic ignored "-Wunused-variable"
-
 namespace synthizer {
 
 // Enough trig that this is justified.
 using namespace std;
 
-#define IVARS_A const double a = pow(10, dbgain / 40)
-#define IVARS_W0 const double w0 = 2 * PI * omega
-
-/* given omega, dbgain, bw, and q in scope, declare/compute EQ cookbook intermediate variables. */
-#define IVARS                                                                                                          \
-  const double cw0 = cos(w0);                                                                                          \
-  const double sw0 = sin(w0);                                                                                          \
-  const double alpha = sw0 / (2 * q);                                                                                  \
-  const double shelfim = 2 * sqrt(a) * alpha
-
-#define IVARS_Q                                                                                                        \
-  IVARS_A;                                                                                                             \
-  IVARS_W0;                                                                                                            \
-  IVARS
-
-#define Q_S                                                                                                            \
-  const double q_r = sqrt((a + 1 / a) * (1 / s - 1) + 2);                                                              \
-  const double q = 1 / q_r
-
-#define Q_BW                                                                                                           \
-  const double q_r = 2 * sinh(log(2) / 2 * bw * w0 * sin(w0));                                                         \
-  const double q = 1 / q_r
-
-#define IVARS_S                                                                                                        \
-  IVARS_W0;                                                                                                            \
-  Q_S;                                                                                                                 \
-  IVARS
-
-#define IVARS_BW                                                                                                       \
-  IVARS_W0;                                                                                                            \
-  Q_BW;                                                                                                                \
-  IVARS_A;                                                                                                             \
-  IVARS
+#define VAR_W0 const double w0 = 2 * PI * omega
+#define VAR_CW0 const double cw0 = cos(w0)
+#define VAR_SW0 const double sw0 = sin(w0)
 
 static BiquadFilterDef makeRet(double b0, double b1, double b2, double a0, double a1, double a2) {
   BiquadFilterDef ret;
@@ -61,8 +29,11 @@ static BiquadFilterDef makeRet(double b0, double b1, double b2, double a0, doubl
 #define RET return makeRet(b0, b1, b2, a0, a1, a2);
 
 BiquadFilterDef designAudioEqLowpass(double omega, double q) {
-  const double dbgain = 0;
-  IVARS_Q;
+  VAR_W0;
+  VAR_SW0;
+  VAR_CW0;
+  const double alpha = sw0 / (2 * q);
+
   const double b0 = (1 - cw0) / 2;
   const double b1 = 1 - cw0;
   const double b2 = b1 / 2;
@@ -73,8 +44,11 @@ BiquadFilterDef designAudioEqLowpass(double omega, double q) {
 }
 
 BiquadFilterDef designAudioEqHighpass(double omega, double q) {
-  const double dbgain = 0;
-  IVARS_Q;
+  VAR_W0;
+  VAR_CW0;
+  VAR_SW0;
+  const double alpha = sw0 / (2 * q);
+
   const double b0 = (1 + cw0) / 2;
   const double b1 = -(1 + cw0);
   const double b2 = (1 + cw0) / 2;
@@ -85,8 +59,11 @@ BiquadFilterDef designAudioEqHighpass(double omega, double q) {
 }
 
 BiquadFilterDef designAudioEqBandpass(double omega, double bw) {
-  const double dbgain = 0;
-  IVARS_BW;
+  VAR_W0;
+  VAR_SW0;
+  VAR_CW0;
+
+  const double alpha = sw0 * sinh(log(2) / 2 * bw * omega / sw0);
   const double b0 = alpha;
   const double b1 = 0;
   const double b2 = -alpha;
@@ -97,8 +74,12 @@ BiquadFilterDef designAudioEqBandpass(double omega, double bw) {
 }
 
 BiquadFilterDef designAudioEqNotch(double omega, double bw) {
-  const double dbgain = 0;
-  IVARS_BW;
+  VAR_W0;
+  VAR_SW0;
+  VAR_CW0;
+
+  const double alpha = sw0 * sinh(log(2) / 2 * bw * omega / sw0);
+
   const double b0 = 1;
   const double b1 = -2 * cw0;
   const double b2 = 1;
@@ -109,8 +90,10 @@ BiquadFilterDef designAudioEqNotch(double omega, double bw) {
 }
 
 BiquadFilterDef designAudioEqAllpass(double omega, double q) {
-  const double dbgain = 0;
-  IVARS_Q;
+  VAR_W0;
+  VAR_CW0;
+  VAR_SW0;
+  const double alpha = sw0 / (2 * q);
   const double b0 = 1 - alpha;
   const double b1 = -2 * cw0;
   const double b2 = 1 + alpha;
@@ -121,7 +104,12 @@ BiquadFilterDef designAudioEqAllpass(double omega, double q) {
 }
 
 BiquadFilterDef designAudioEqPeaking(double omega, double bw, double dbgain) {
-  IVARS_BW;
+  VAR_W0;
+  VAR_CW0;
+  VAR_SW0;
+
+  const double a = pow(10, dbgain / 40);
+  const double alpha = sw0 * sinh(log(2) / 2 * bw * omega / sw0);
   const double b0 = 1 + alpha * a;
   const double b1 = -2 * cw0;
   const double b2 = 1 - alpha * a;
@@ -132,32 +120,36 @@ BiquadFilterDef designAudioEqPeaking(double omega, double bw, double dbgain) {
 }
 
 BiquadFilterDef designAudioEqLowShelf(double omega, double db_gain, double s) {
-  /* Note: these are actually different from all the others. Not using the macros isn't a mistake. */
-  const double a = pow(10, db_gain / 40.0);
-  const double w0 = 2 * PI * omega;
-  const double cw0 = cos(w0);
-  const double sw0 = sin(w0);
-  const double alpha = sqrt((a * a + 1) / s - (a - 1) * (a - 1));
+  VAR_W0;
+  VAR_CW0;
+  VAR_SW0;
 
-  const double b0 = a * ((a + 1) - (a - 1) * cw0 + alpha * sw0);
+  const double a = pow(10, db_gain / 40.0);
+
+  const double beta = sqrt(a) * sqrt((a + 1 / a) * (1 / s - 1) + 2);
+
+  const double b0 = a * ((a + 1) - (a - 1) * cw0 + beta * sw0);
   const double b1 = 2 * a * ((a - 1) - (a + 1) * cw0);
-  const double b2 = a * ((a + 1) - (a - 1) * cw0 - alpha * sw0);
-  const double a0 = (a + 1) + (a - 1) * cw0 + alpha * sw0;
+  const double b2 = a * ((a + 1) - (a - 1) * cw0 - beta * sw0);
+  const double a0 = (a + 1) + (a - 1) * cw0 + beta * sw0;
   const double a1 = -2 * ((a - 1) + (a + 1) * cw0);
-  const double a2 = (a + 1) + (a - 1) * cw0 - alpha * sw0;
+  const double a2 = (a + 1) + (a - 1) * cw0 - beta * sw0;
   RET;
 }
 
 BiquadFilterDef designAudioEqHighShelf(double omega, double db_gain, double s) {
-  /* Note: these are actually different from all the others. Not using the macros isn't a mistake. */
   const double a = pow(10, db_gain / 40.0);
-  IVARS_S;
-  const double b0 = a * ((a + 1) + (a - 1) * cw0 + shelfim);
+  VAR_W0;
+  VAR_SW0;
+  VAR_CW0;
+  const double beta = sqrt(a) * sqrt((a + 1 / a) * (1 / s - 1) + 2);
+
+  const double b0 = a * ((a + 1) + (a - 1) * cw0 + beta * sw0);
   const double b1 = -2 * a * ((a - 1) + (a + 1) * cw0);
-  const double b2 = a * ((a + 1) + (a - 1) * cw0 - shelfim);
-  const double a0 = (a + 1) - (a - 1) * cw0 + shelfim;
+  const double b2 = a * ((a + 1) + (a - 1) * cw0 - beta * sw0);
+  const double a0 = (a + 1) - (a - 1) * cw0 + beta * sw0;
   const double a1 = 2 * ((a - 1) - (a + 1) * cw0);
-  const double a2 = (a + 1) - (a - 1) * cw0 - shelfim;
+  const double a2 = (a + 1) - (a - 1) * cw0 - beta * sw0;
   RET;
 }
 
