a95e103a11b83c71f199e97370210ddd3556e2ed
Austin Hicks <ahicks@ahicks.io>
2022-09-18 17:29:31 -0700

    Update synthizer again

--- synthizer-sys/synthizer-vendored/include/synthizer/generators/buffer.hpp
+++ synthizer-sys/synthizer-vendored/include/synthizer/generators/buffer.hpp
@@ -174,69 +174,101 @@ inline void BufferGenerator::generateNoPitchBend(float *output, FadeDriver *gd) 
       mp);
 }
 
-inline void BufferGenerator::generatePitchBend(float *output, FadeDriver *gd) const {
-  assert(this->finished == false);
+namespace buffer_generator_detail {
 
-  // We have some fractional offset of the position, which we need to use to know how far from the first sample of this
-  // slice over the buffer is.
-  const std::uint64_t offset = this->scaled_position_in_frames % config::BUFFER_POS_MULTIPLIER;
+/**
+ * Parameters needed to do pitch bend.
+ *
+ * If pitch bend can't be done because delta is 0, this is zero-initialized and iterations = 0.
+ * */
+struct PitchBendParams {
+  std::uint64_t offset;
+  std::size_t iterations;
 
-  const std::uint64_t delta = this->scaled_position_increment;
+  /**
+   * The span we must grab from the underlying buffer, in samples.
+   *
+   * Possibly includes the implicit zero, if required.
+   * */
+  std::size_t span_start, span_len;
 
-  // If delta is 0 then computing our iterations can divide by zero, and it'd be 0 movement anyway.
-  if (delta == 0) {
-    return;
+  /**
+   * Whether or not the buffer should include the implicit zero.
+   * */
+  bool include_implicit_zero;
+};
+
+inline PitchBendParams computePitchBendParams(std::uint64_t scaled_position, std::uint64_t delta,
+                                              std::uint64_t buffer_len_no_zero, bool looping) {
+  PitchBendParams ret{};
+
+  if (delta == 0 || scaled_position >= buffer_len_no_zero) {
+    ret.iterations = 0;
+    return ret;
   }
 
-  // This is a bit complicated.  First, we will read up to 1 more than the block size * delta.  But if that's past the
-  // end, we will instead read up to the end, and truncate our pitch bend early.
-  //
-  // Don't forget that the fractional part of the position contributes.
-  std::uint64_t scaled_read_span_tmp = offset + config::BLOCK_SIZE * delta;
-  // the actual bound is the scaled span's ceil.  If this is exactly equal to the scaled span, we must go one more
-  // sample: even though the below loop would use a 0 inbterpolation weight, it's stil going to try to read one after
-  // the end.
-  std::uint64_t scaled_read_span = ceilByPowerOfTwo(scaled_read_span_tmp, config::BUFFER_POS_MULTIPLIER);
-  scaled_read_span =
-      (scaled_read_span == scaled_read_span_tmp) ? scaled_read_span + config::BUFFER_POS_MULTIPLIER : scaled_read_span;
-  // Note that in the case where we had to bump scaled_read_span up by a sample, it's already an exact multiplier, so
-  // floor is fine.
-  std::uint64_t read_span = scaled_read_span / config::BUFFER_POS_MULTIPLIER;
+  ret.iterations = config::BLOCK_SIZE;
 
-  std::size_t loop_iterations = config::BLOCK_SIZE;
-
-  // If the spamn is going to read past the end and we aren't looping, we must bring it down.
-  if (this->getLooping() == false) {
-    // Recall that the span can read into the implicit zero without issue.
-    if (this->getPosInSamples() + read_span >= this->reader.getLengthInFrames(true)) {
-      // When figuring out what's available, we must not include the implicit zero.  That is, available is the maximum
-      // movement of the lower sample in the interpolation.
-      std::uint64_t available = this->reader.getLengthInFrames(false) - this->getPosInSamples() - 1;
-      // We know read_span was going to read past the end, and thus that available is less than that. Available
-      // doesn't include the implicit zero, so we add it back.
-      read_span = available + 1;
-      // But the total number of iterations is based only on scaling available, in this case.
-      loop_iterations = available * config::BUFFER_POS_MULTIPLIER / delta;
+  // If we are going to read past the end and are not looping, we must do less than that.
+  if (looping == false) {
+    // if the lower sample in the linear interpolation is going past the end of the buffer, more care is required.
+    if (scaled_position + ret.iterations * delta >= buffer_len_no_zero) {
+      // We must work out how many fractional samples remain before the lower sample hits the end of the buffer.
+      std::uint64_t remaining_data = buffer_len_no_zero - scaled_position - 1;
+      // If the available remaining data perfectly divides by delta, then the loop iterations is simply the division.
+      if (remaining_data % delta == 0) {
+        ret.iterations = remaining_data / delta;
+      } else {
+        // Otherwise, we can actually run one more iteration than it seems like we should be able to because the lower
+        // sample is still fine.
+        //
+        // This is effectively ceil, but written out for clarity.
+        ret.iterations = remaining_data / delta + 1;
+      }
     }
   }
 
-  // Compilers are bad about telling that channels doesn't change.
-  const unsigned int channels = this->getChannels();
+  ret.include_implicit_zero = looping == false;
+  ret.offset = scaled_position - floorByPowerOfTwo(scaled_position, config::BUFFER_POS_MULTIPLIER);
 
-  // if we aren't looping, we include the implicit zero so that we can go slightly past the end of the buffer if needed,
-  auto mp = this->reader.getFrameSlice(this->getPosInSamples(), read_span, this->getLooping() == false, true);
+  // We can work out the read span from the number of iterations.
+  ret.span_start = scaled_position / config::BUFFER_POS_MULTIPLIER;
+
+  // The upper end of the range is the floor of the uppermost lower sample, plus 1.
+  ret.span_len = (ret.offset + (ret.iterations - 1) * delta) / config::BUFFER_POS_MULTIPLIER + 1;
+
+  return ret;
+}
+} // namespace buffer_generator_detail
+
+inline void BufferGenerator::generatePitchBend(float *output, FadeDriver *gd) const {
+  assert(this->finished == false);
+
+  const auto params = buffer_generator_detail::computePitchBendParams(
+      this->scaled_position_in_frames, this->scaled_position_increment,
+      this->reader.getLengthInFrames(false) * config::BUFFER_POS_MULTIPLIER, this->getLooping());
+
+  if (params.iterations == 0) {
+    return;
+  }
+
+  auto mp = this->reader.getFrameSlice(params.span_start, params.span_len, params.include_implicit_zero, true);
 
   // In the case where the number of iterations is the block size, we can use VBool to let the compiler know.
-  bool _is_full_block = loop_iterations == config::BLOCK_SIZE;
+  bool _is_full_block = params.iterations == config::BLOCK_SIZE;
+
+  // the compiler is bad about telling that channels doesn't change.
+  const unsigned int channels = this->getChannels();
 
   std::visit(
       [&](auto ptr, auto full_block) {
         gd->drive(this->getContextRaw()->getBlockTime(), [&](auto gain_cb) {
           // Let the compiler understand that, sometimes, it can fully unroll the loop.
-          const std::size_t iters = full_block ? config::BLOCK_SIZE : loop_iterations;
+          const std::size_t iters = full_block ? config::BLOCK_SIZE : params.iterations;
+          const std::uint64_t delta = this->scaled_position_increment;
 
           for (std::size_t i = 0; i < iters; i++) {
-            std::uint64_t scaled_effective_pos = offset + delta * i;
+            std::uint64_t scaled_effective_pos = params.offset + delta * i;
             std::size_t scaled_lower = floorByPowerOfTwo(scaled_effective_pos, config::BUFFER_POS_MULTIPLIER);
             std::size_t lower = scaled_lower / config::BUFFER_POS_MULTIPLIER;
 
